53fee46f06cbf17a3d65be0a52627ee64af1f778
unknown <Inam Ur Rehman@Inam-Rehman.ad.nayatel.com>
2025-12-27 01:41:26 +0500

    Updated api/axigen.js

--- web/src/api/axigen.js
+++ web/src/api/axigen.js
@@ -2,24 +2,24 @@ import api from "./client";
 
 // GET /servers
 export async function getServers() {
-  const { data } = await api.get("/servers");
+  const { data } = await api.get("/servers/");
   return data;
 }
 
 // GET /domains/server/<server_id>
 export async function getDomainsByServer(serverId) {
-  const { data } = await api.get(`/domains/server/${serverId}`);
+  const { data } = await api.get(`/domains/server/${serverId}/`);
   return data;
 }
 
 // GET /accounts/domain/<domain_id>
 export async function getAccountsByDomain(domainId) {
-  const { data } = await api.get(`/accounts/domain/${domainId}`);
+  const { data } = await api.get(`/accounts/domain/${domainId}/`);
   return data;
 }
 
 // GET /summary
 export async function getSummary() {
-  const { data } = await api.get("/summary");
+  const { data } = await api.get("/summary/");
   return data;
 }
